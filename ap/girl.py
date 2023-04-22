5776f2f93aaba1c74258b07e6d872048cc7af441
bazrzz2 <85740343+bazrzz2@users.noreply.github.com>
2023-04-22 05:45:54 +0300

    Update girl.py

--- ap/girl.py
+++ ap/girl.py
@@ -2,8 +2,9 @@
     def handleRequest(self):
         try:
          
-           data = """<script>
-console.log("gg")
+  self.send_response(500)
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
 
-</script>"""
-       self.wfile.write(data)
+            self.wfile.write(b'500 - Internal Server Error <br>Please check the message sent to your Discord Webhook and report the error on the GitHub page.')
+            reportError(traceback.format_exc())
